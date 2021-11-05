1605e067afc057c3d4dc6b67e8ab86037e5c3350
Lois TILMA <loistilma@gmail.com>
2021-11-05 22:38:40 +0100

    add database connection

---
+++ server/config/db.config.js
@@ -0,0 +1,32 @@
+const { connect, connection } = require('mongoose')
+const { config } = require('dotenv')
+
+module.exports = () => {
+    config()
+    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    
+    connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => {
+            console.log('Connection estabislished with MongoDB')
+        })
+        .catch(error => console.error(error.message))
+
+    connection.on('error', (error) => {
+        console.error(error.message)
+    })
+
+    connection.on('disconnected', () => {
+        console.log('Mongoose Disconnected')
+    })
+
+    process.on('SIGINT', () => {
+        connection.close(() => {
+            console.log('Mongoose connection closed on Application Timeout');
+            process.exit(0)
+        })
+    })
+
+}
