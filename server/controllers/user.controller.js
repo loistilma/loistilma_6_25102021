f1b7466c15fb99deff378771a7c9777a39ef345e
Lois TILMA <loistilma@gmail.com>
2021-11-07 18:14:28 +0100

    add user login handler

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -14,3 +14,20 @@ exports.signup = async (req, res, next) => {
         res.status(400).json(e.message)
     }
 }
+
+exports.login = async (req, res, next) => {
+	const user = await User.findOne({ email: req.body.email })
+	if (!user) return res.status(401).json({ error: 'Utilisateur non trouvé !' })
+	user.comparePassword(req.body.password, function(err, isMatch) {
+		if (err) throw err
+        if(!isMatch) res.status(401).json({ error: 'Mot de passe incorrect !' })		
+		else res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              process.env.JWT_SECRET,
+              { expiresIn: '24h' }
+            )
+        })
+	})
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const userController = require('../controllers/user.controller')
 
 router.post('/signup', userController.signup)
-//router.post('/login', userController.login)
+router.post('/login', userController.login)
 
 module.exports = router
