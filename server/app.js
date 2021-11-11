b0d8a74d0d18a82a92a7a0e70e2fdfad0210bab4
Lois TILMA <loistilma@gmail.com>
2021-11-11 12:53:00 +0100

    Uninstall morgan and cookie-parser

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 var express = require('express')
 var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
 var cors = require('cors')
 const { handleError } = require('./helpers/error.helper')
 
@@ -19,11 +17,9 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN
 }))
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use('/api/auth', usersRouter)
 app.use('/api/sauces', saucesRouter)
